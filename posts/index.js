ccf38d397e90dd280beed27a9d72675b6a64b6a4
Sailesh Dahal <saileshbro@gmail.com>
2020-12-24 21:17:37 +0545

    added k8s services and deployment objects, nodeport and clusterip

--- posts/index.js
+++ posts/index.js
@@ -13,7 +13,7 @@ app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex')
   const { title } = req.body
   posts[id] = { id, title }
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id,
@@ -24,5 +24,6 @@ app.post('/posts', async (req, res) => {
 })
 app.post('/events', (req, res) => {})
 app.listen(4000, () => {
+  console.log('v55')
   console.log('Listening on 4000')
 })
